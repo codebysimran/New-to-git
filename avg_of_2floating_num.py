2d1c590731d1be0925a9618a5d57ef59ae9dcb98
codebysimran <25amtics299@gmail.com>
2026-05-20 09:37:47 +0530

    Add files via upload
    7 files added.

---
+++ avg_of_2floating_num.py
@@ -0,0 +1,4 @@
+a= float(input("Enter a="))
+b= float(input("Enter b="))
+c= (a+b)/2
+print("The average of a and b is:-",c)
---
+++ count$.py
@@ -0,0 +1,3 @@
+str = "$ have so many $ that $ is the best $ in the world. $ is so good that everyone loves $."
+count = str.count("$")
+print("count", count)
